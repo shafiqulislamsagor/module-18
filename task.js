64a3e3169e81b3110bda4a9923a9b95b97bb6fa0
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-01-30 02:34:39 +0600

    loop continue

--- task.js
+++ task.js
@@ -6,4 +6,16 @@ for(let i = even ; i <= 40 ; i++){
         continue;
     }
     console.log(i)
+    console.log('completed')
+}
+
+// task 2 
+
+let odd = 55 ;
+
+for(let i = odd ; i <= 85 ; i++){
+    if(i % 5 !== 0){
+        continue;
+    }
+    console.log(i)
 }
