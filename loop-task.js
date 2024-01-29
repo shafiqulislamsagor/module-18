2ea4e6c3f1091b64bc643be5a54be1227b91c8f5
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-01-29 21:41:25 +0600

    loop-task 2

--- loop-task.js
+++ loop-task.js
@@ -1,6 +1,35 @@
+// Task 1
 let point = 1 ;
 
 while(point <= 60){
     console.log("I will invest at least 6 hrs every single day for next 60 days!",point);
+    if( point % 10 === 0){
+        console.log('your task complete')
+    }
     point++;
+}
+// Task 2
+
+// Subtask 1
+
+let obb = 61 ;
+
+while(obb <= 100){
+    console.log('obb no - ', obb);
+    if(obb % 2 != 0 && obb == 99){
+        console.log('your subtask completed')
+    }
+    obb += 2 ;
+}
+
+// Subtask 2
+
+let even = 78 ;
+
+while(even <=98){
+    console.log('even no - ',even);
+    if(even % 2 ==0 && even == 98){
+        console.log('Your subtask completed')
+    }
+    even += 2;
 }
