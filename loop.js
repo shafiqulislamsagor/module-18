3183ca9caa43d037349f332bbee5558c25002bed
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-01-29 19:05:52 +0600

    module 18-1

---
+++ loop.js
@@ -0,0 +1,11 @@
+const fruits = ['Apple','Orange','Banana','Mango'];
+
+for(const fruit of fruits){
+    console.log(fruit);
+}
+
+const numbers = [1,2,3,4,5,6,7,8,9];
+
+for(const num of numbers){
+    console.log(num);
+}
